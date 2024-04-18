55305feb85a4e96cb043126402cff2ccaf1f83cc
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-19 02:05:57 +0930

    test file.

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,8 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		return Response.json({
+			message: "you did not send a get request"
+		})
 	},
 };
